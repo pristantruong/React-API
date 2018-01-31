aae29ee6cafa67a10be1c23ac41c3e71f2d460cc
Sang <pristan106@gmail.com>
2018-01-31 20:53:23 +0700

    Axios connect to get API

--- src/pages/ProductListPage/ProductListPage.js
+++ src/pages/ProductListPage/ProductListPage.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ProductList from './../../components/ProductList/ProductList';
 import ProductItem from './../../components/ProductItem/ProductItem';
 import {connect} from 'react-redux';
+import axios from 'axios';
 
 class ProductListPage extends Component {
 
@@ -22,7 +23,19 @@ class ProductListPage extends Component {
     }
 
     render() {
-        var {products} = this.props;
+        // var {products} = this.props;
+        var products = [];
+        axios({
+            method: 'GET',
+            url : 'http://localhost:3000/products',
+            data: null,
+        }).then(res => {
+            console.log(res);
+            products = res.data;
+        }).catch(err => {
+            console.log(err);
+        }); 
+        
         return (
             <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
                 <button type="button" className="btn btn-info mb-10">Add</button>
