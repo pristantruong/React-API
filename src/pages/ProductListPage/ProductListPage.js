5bede3a4b341568fa5bf583837a8392bb0fb5fc0
Sang <pristan106@gmail.com>
2018-01-31 22:27:09 +0700

    Sperate Axios

--- src/pages/ProductListPage/ProductListPage.js
+++ src/pages/ProductListPage/ProductListPage.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
 import ProductList from './../../components/ProductList/ProductList';
 import ProductItem from './../../components/ProductItem/ProductItem';
-import {connect} from 'react-redux';
-import axios from 'axios';
+import { connect } from 'react-redux';
+import callApi from './../../utils/apiCaller';
 
 class ProductListPage extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            products: []
+        };
+    }
+
+    componentDidMount() {
+       callApi('products', 'GET', null).then(res => {
+           this.setState({
+               products : res.data
+           })
+       })
+    }
+
 
     showProduct = (products) => {
         var result = null;
@@ -24,18 +40,7 @@ class ProductListPage extends Component {
 
     render() {
         // var {products} = this.props;
-        var products = [];
-        axios({
-            method: 'GET',
-            url : 'http://localhost:3000/products',
-            data: null,
-        }).then(res => {
-            console.log(res);
-            products = res.data;
-        }).catch(err => {
-            console.log(err);
-        }); 
-        
+        var {products} = this.state;
         return (
             <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
                 <button type="button" className="btn btn-info mb-10">Add</button>
