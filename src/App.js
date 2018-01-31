0a7c7b06590a9197c0e9793cdc4706ee70f723c8
Sang <pristan106@gmail.com>
2018-01-31 14:38:57 +0700

    Template

--- src/App.js
+++ src/App.js
@@ -1,21 +1,77 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <div className="navbar navbar-default">
+                    <a className="navbar-brand" >API</a>
+                    <ul className="nav navbar-nav">
+                        <li className="active">
+                            <a>Home</a>
+                        </li>
+                        <li>
+                            <a>Course</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div className="container">
+
+                    <div className="row">
+                        <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+
+                            <button type="button" className="btn btn-info mb-10">Add</button>
+
+                            <div className="panel panel-primary">
+                                <div className="panel-heading">
+                                    <h3 className="panel-title">List Course</h3>
+                                </div>
+                                <div className="panel-body">
+                                    <table className="table table-bordered table-hover">
+                                        <thead>
+                                            <tr>
+                                                <th>#</th>
+                                                <th>ID</th>
+                                                <th>Name</th>
+                                                <th>Price</th>
+                                                <th>Status</th>
+                                                <th>Action</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>1</td>
+                                                <td>1</td>
+                                                <td>NodeJS</td>
+                                                <td>500</td>
+                                                <td>
+                                                    <span className="label label-success">Active</span>
+                                                </td>
+                                                <td>
+                                                    
+                                                    <button type="button" className="btn btn-info mr-10 btn-xs">Edit</button>
+                                                    
+                                                    <button type="button" className="btn btn-danger btn-xs">Delete</button>
+                                                    
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+
+        );
+    }
 }
 
 export default App;
